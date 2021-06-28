4006d4fb40aa41f1ff539385ea1535e310335f2c
Kushagra Sharma <kushagra2508sharma@gmail.com>
2021-06-28 09:44:40 +0530

    Final Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+// creating server by this way help us to reuse the server instance to run socket.io
+// we can also do this by app.listen as it also gives server instance
+// the main difference is that here we are creating server on our own on the other hand
+// express creates the server for us when we use app.listen
+const io = require("socket.io")(server);// we want socket.io to work on this server
+const { v4: uuidV4 } = require("uuid"); // creates a link to newly created room
+
+
+app.set("view engine", "ejs");// settin EJS as templating engine. EJS looks into view folder
+// template engine enable us to use static template files and loads the value of variables on runtime
+app.use(express.static('public'));// static files will be stored in public folder
+
+app.get("/", (req, res) => { // whenever a user lands on "/" uuid will create a new link and redirects the user to that link 
+	res.redirect(`/${uuidV4()}`); // so that they can have their own room 
+});
+
+app.get("/:room", (req, res) => { // room will contains a param which will be provided by uuid when we first land on "/" url
+	res.render('room', { roomId: req.params.room });
+});
+
+// whenever someone connects to server we will set up an event listner that will fire a callback 
+// function. The parameter of that function will be the socket instance that is made after we connect to the server
+// if 10 clients make connection with the browser they will each have a socket
+io.on("connection", (socket) => {
+	// when everything is set up (we have user) on the frontend we call this "join-room" function 
+	console.log("1");
+	socket.on("join-room", (roomId, userId) => {
+	    //console.log(roomId, userId);
+        console.log("2");
+		// joining new connection to the room with roomId
+		// it makes the matching socket instances join the same room
+		socket.join(roomId); 
+
+		// sending a broadcast msg to everyone(except us) present in the room that a new member has joined 
+		socket.broadcast.to(roomId).emit("user-connected", userId);
+
+		socket.on("disconnect", () => {
+			console.log("3");
+			socket.broadcast.to(roomId).emit("user-disconnected", userId);
+		});
+	});
+})
+
+server.listen(5000);
---
+++ public/script.js
@@ -0,0 +1,148 @@
+// here we have access to room id and all the socket io code which is essential for calling "join-room" function
+
+// socket is connected to root path
+const socket = io("/"); // running on frontend. This io object is globally available
+
+// making a refrence to the Grid which will contain all our videos
+const videoGrid = document.getElementById("video-grid");;
+
+// Making connection with peer server. The peer server will take all the webRTC information of the user and turns it into a userId
+const peer = new Peer(undefined, { // we are passing (id) undefined as we want peerjs to take care of id on its own
+    host: "/",
+    port: "5001"
+});
+
+// getting reference to a video
+const myVideo = document.createElement("video");// creating  video element
+myVideo.muted = true; // so that we don't here our own voice doesn't mute for other people
+
+const peers ={} // object for storing the call
+
+let myVideoStream // object for storing the media stream (A stream of media content consisting of several video and audio track)
+// connecting our video
+navigator.mediaDevices.getUserMedia({// it prompts the user for permission to use a media input which produces a MediaStream
+    // we want audio and video
+    video: true,
+    audio: true,
+}).then(stream => {// stream is going to be our video and audio and we want our video object to use that stream (myVideo)
+    console.log("5");
+    myVideoStream = stream;
+    addVideoStream(myVideo, myVideoStream);// adding our video stream
+
+    // to get to see other video we want to take the call which the other user sends
+    peer.on("call", (call) => {
+        call.answer(stream); // for us to see their video
+        console.log("11");
+        // this way the other user can also see our video
+        const video = document.createElement("video");
+        call.on("stream", userVideoStream => {
+            console.log("12");
+            addVideoStream(video, userVideoStream)// adding other users' video stream
+        });
+    });
+
+    // listner for new member connection
+    socket.on("user-connected", (userId) => {
+    //console.log("User id is: ", userId);
+    // sending our video stream to new user
+    //connectToNewUser(userId, stream);
+    console.log("7");
+    // sending the existing stream to new user 
+    setTimeout(connectToNewUser,1000,userId,stream);// to resolve the race problem
+});
+})
+
+socket.on("user-disconnected", userId => {
+    //console.log(userId);
+    if(peers[userId]){
+        console.log("14");
+        peers[userId].close(); // closing connection with the user
+    }
+});
+
+// as soon as we connects to peer server we will join our user to a room
+peer.on("open", id => {
+    // calling join room function in index.js
+    console.log("8");
+    socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+    // calling user with id "userId" and giving them our stream
+    console.log("9");
+    const call = peer.call(userId, stream);// sending our stream to the new user
+
+    // and after that they will send their video stream which we want to add to ours
+    const video = document.createElement("video");
+    call.on("stream", userVideoStream => {
+        console.log("10");
+        addVideoStream(video, userVideoStream);
+    });
+
+    // whenever person leaves we want to disconnect their video
+    call.on("close", () => {
+        console.log("13");
+        video.remove();
+    });
+
+    // every userId is linked to the call we make with them
+    console.log("prevoius ", peers);
+    peers[userId] = call;
+    console.log("new ", peers);
+}
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream; // setting the mediaStream of video object
+    // this will allow us to play our video
+    console.log("6");
+    video.addEventListener("loadedmetadata", () => {
+        video.play(); // once the stream is loaded play the video
+    });
+    console.log("video ", video);
+    videoGrid.append(video); // adding new video to videoGrid
+}
+
+// Mute Unmute functionality
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled; // gives a boolean value
+    if (enabled) { // if its true mute it
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => { // change the font icon
+    const html = `<i class="fas fa-microphone"></i><span>Mute</span>`
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+  
+const setUnmuteButton = () => { // change the font icon
+    const html = `<i class="unmute fas fa-microphone-slash"></i><span>Unmute</span>`
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+// Play Stop
+const playStop = () => {
+    //console.log('object')
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo()
+    } else {
+      setStopVideo()
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setStopVideo = () => {
+    const html = `<i class="fas fa-video"></i><span>Stop Video</span>`
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+  
+const setPlayVideo = () => {
+    const html = `<i class="stop fas fa-video-slash"></i><span>Play Video</span>`
+    document.querySelector('.main__video_button').innerHTML = html;
+}
